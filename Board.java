c4c20310ca87eec8528d9a5788c855ca8c52687d
Brady O'Connor <oconnbra25@ad.7hills.org>
2024-11-04 08:40:47 -0500

    test

--- Board.java
+++ Board.java
@@ -1,1 +1,12 @@
 //board file
+
+//whats in the box
+	//5 ships
+	//carrier has 5 pegs
+	
+	//2 boards
+		//rows and columns
+		//10x10
+		//letters down the side
+		//numbers across the top
+	//2 colored set of pegs
