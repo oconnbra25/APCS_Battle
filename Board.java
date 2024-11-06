0407f0ce4566ecf21005146c5d9eb1b0439035c9
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2024-11-06 08:28:29 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -10,3 +10,61 @@
 		//letters down the side
 		//numbers across the top
 	//2 colored set of pegs
+
+
+public class Board
+{
+	int[][] boardShips = new int[10][10];
+	int[][] boardNotes = new int[10][10];
+	
+	public static void main(String[] args)
+	{
+		toString();
+	}
+	
+	public void toString(int[][] boardShips, int[][] boardNotes)
+	{
+		int r = 0;
+		int c = 0;
+		
+		//making the printout for your ships
+		System.out.println("# A B C D E F G H I J");
+		while (r < 10)
+		{
+			while (c < 10)
+			{
+				System.out.print(boardShips[r][c] + " ");
+				c++;
+			}
+			
+			r++;
+			System.out.print("\n" + r + " "); //prints the lft side of the column to show what row its in and moves the row down
+			c = 0;
+		}
+		
+		//making the printout for your notes
+		System.out.println("# A B C D E F G H I J");
+		while (r < 10)
+		{
+			while (c < 10)
+			{
+				System.out.print(boardNotes[r][c] + " ");
+				c++;
+			}
+			
+			r++;
+			System.out.print("\n" + r + " "); //prints the lft side of the column to show what row its in and moves the row down
+			c = 0;
+		}
+	}
+}
+		
+	/*	
+	public checkShip(int r, int c)
+	{
+		if (boardShips[r][c] == 1) //one is currently a placeholder for a value that indicates whether or not the ship is there
+		{
+			System.out.println("Hit");
+		}
+	}
+*/
