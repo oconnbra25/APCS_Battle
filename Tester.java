31f70e7ef2385f64430e63ae103afff36709df30
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2024-11-06 09:23:31 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,2 +1,14 @@
 //Main driver or tester
 //test
+public class Tester
+{
+	public static void main(String[] args)
+	{
+			Board player1 = new Board();
+			
+			player1.toString();
+	}
+}
+			
+			
+	
