f341a08cdcc986c557d2460a7db522ba39ecedf6
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2024-11-18 09:07:33 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -6,7 +6,13 @@ public class Tester
 	{
 			Board player1 = new Board();
 			
-			player1.toString();
+			player1.placeShip(2, 1, 1, "East");
+			player1.placeShip(3, 0, 0, "South");
+			player1.placeShip(3, 5, 0, "East");
+			player1.placeShip(4, 9, 0, "East");
+			player1.placeShip(5, 6, 4, "West");
+			
+			player1.printB();
 	}
 }
 			
